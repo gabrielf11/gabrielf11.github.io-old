300a9c7338ab3499e525041841c704974558baf4
GabrielF11 <88408682+GabrielF11@users.noreply.github.com>
2021-10-16 12:26:08 -0700

    console secrets (;

---
+++ console.js
@@ -0,0 +1,10 @@
+//I learned JS (well pretty much only variables) soooooooooo yeah. This is the result. 
+//The first bit of JS in this website actually written by me.
+const isDepressed = true;
+const isTired = true;
+const websiteIsCool = true;
+const happy = 'it depends i guess...';
+const imBored = 'ok, check out https://imbored.gabrielf11.ga'
+console.log('Hello! Welcome to the console! I wonder what you are doing here :thinking:')
+console.log('Please do not stay here long, as this console is hidden with secrets...')
+console.log('DANG IT GABRIELF11 TOLD ME NOT TO TELL YOU THAT! Just- just leave, NOW!')
